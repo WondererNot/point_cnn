5d91ba21298f2eb7a276e4a7f2315a418b5a5b51
WondererNot <wayne_ong95@hotmail.com>
2020-01-24 17:01:48 +0800

    Prevent data download

--- provider.py
+++ provider.py
@@ -4,7 +4,7 @@ import numpy as np
 import h5py
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
-
+"""
 # Download dataset for point cloud classification
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 if not os.path.exists(DATA_DIR):
@@ -15,7 +15,7 @@ if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
   os.system('wget %s; unzip %s' % (www, zipfile))
   os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
   os.system('rm %s' % (zipfile))
-
+"""
 
 def shuffle_data(data, labels):
   """ Shuffle data and labels.
